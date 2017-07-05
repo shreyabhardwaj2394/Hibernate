3c8ddd735fdf7d093470d3f8b902aae8297e4afc
shreyabhardwaj2394 <shreya2394bhardwaj@gmail.com>
2017-07-06 00:32:37 +0530

    question 4

--- hibernate-bootcamp-master/hibernate-bootcamp-master/src/main/java/com/hibernate/exercise/Application.java
+++ hibernate-bootcamp-master/hibernate-bootcamp-master/src/main/java/com/hibernate/exercise/Application.java
@@ -17,13 +17,46 @@ public class Application {
 
         SessionFactory sessionFactory=new Configuration().configure().buildSessionFactory();
         Session session=sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
+        try {
+            Transaction transaction = session.beginTransaction();
+            Author author1=new Author();
+            author1.setFirstName("shreya");
+            author1.setLastName("bhardwaj");
+            author1.setAge(22);
+            author1.setDate(new GregorianCalendar(1994,10,23).getTime());
+            session.save(author1);
 
-        transaction.commit();
-        session.close();
-        sessionFactory.close();
+
+            Author author2=new Author();
+            author2.setFirstName("rahul");
+            author2.setLastName("gagan");
+            author2.setAge(25);
+            author2.setDate(new GregorianCalendar(1991,10,23).getTime());
+            session.save(author2);
+
+            Author author3=new Author();
+            author3.setFirstName("skand");
+            author3.setLastName("bhardwaj");
+            author3.setAge(45);
+            author3.setDate(new GregorianCalendar(1994,10,29).getTime());
+            session.save(author3);
+
+            Author author4=new Author();
+            author4.setFirstName("abhimanyu");
+            author4.setLastName("aggarwal");
+            author4.setAge(24);
+            author4.setDate(new GregorianCalendar(1992,11,29).getTime());
+            session.save(author4);
+
+            transaction.commit();
+        }catch (RuntimeException e){
+            System.out.println("----------------------------");
+        }
+        finally {
+            session.close();
+            sessionFactory.close();
+        }
         System.out.println("Done");
-
 
     }
 }
