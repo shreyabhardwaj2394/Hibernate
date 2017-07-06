68b6e89b2cc43e7aa3ff762a4bc64816d0cad05f
shreyabhardwaj2394 <shreya2394bhardwaj@gmail.com>
2017-07-06 21:08:16 +0530

    question 10,added class address

---
+++ hibernate-bootcamp-master/hibernate-bootcamp-master/src/main/java/com/hibernate/exercise/Address.java
@@ -0,0 +1,37 @@
+package com.hibernate.exercise;
+
+/**
+ * Created by Shreya on 7/6/2017.
+ */
+
+
+//Create a class Address
+public class Address {
+    int streetNumber;
+    String location;
+    String state;
+
+    public int getStreetNumber() {
+        return streetNumber;
+    }
+
+    public void setStreetNumber(int streetNumber) {
+        this.streetNumber = streetNumber;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+}
